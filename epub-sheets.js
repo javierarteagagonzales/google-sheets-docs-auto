dadc2ca18b7c2e9985cbd3be2a1f9eb008a688c1
Javier Alejandro Arteaga Gonzales <132113806+javierarteagagonzales@users.noreply.github.com>
2024-02-21 15:59:41 -0500

    create epub en sheets

---
+++ epub-sheets.js
@@ -0,0 +1,32 @@
+function Listar_Archivos_OE_nivelEpub() {
+    try {
+        var hoja = SpreadsheetApp.getActive().getSheetByName("hoja1");
+
+        if (!hoja) {
+            throw new Error("La hoja con el nombre 'PDF' no se encontró.");
+        }
+
+        var cabecera = hoja.getRange("A1:G1");
+        var Datoscabecera = cabecera.setValues([["Nombre", "ID-archivo", "tipo", "url-archivo", "Folder", "Url-folder", "Tamaño"]]);
+
+        var folder = DriveApp.getFolderById('1lH2Q7cNnoo958R5A3MJVsgS3-vS-4_PD');
+        var Mis_Archivos = folder.searchFiles('mimeType = "application/epub+zip" ');
+
+        while (Mis_Archivos.hasNext()) {
+            var Archivo = Mis_Archivos.next();
+            var parentFolder = Archivo.getParents().next();
+
+            hoja.appendRow([
+                Archivo.getName(),
+                Archivo.getId(),
+                Archivo.getMimeType(),
+                Archivo.getUrl(),
+                parentFolder.getName(),
+                parentFolder.getUrl(),
+                (Archivo.getSize() * 0.000977).toFixed() + " KB"
+            ]);
+        }
+    } catch (error) {
+        console.error("Error en el script:", error.message);
+    }
+}
